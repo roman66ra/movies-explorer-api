5ad2b7f06beb1ac9d65c67707a7ba7ec2a75c43e
Roman Rafikov <romanrara@yandex.ru>
2023-09-17 16:35:22 +0500

    backend

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const routes = require('./routes/index');
 
 const app = express();
 app.use(cors());
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3002, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
